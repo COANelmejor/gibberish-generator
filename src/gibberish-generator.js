fa069f714456a593334f55477d09361818e17ff0
Christian Arana <coanelmejor@gmail.com>
2022-03-02 10:55:51 -0600

    Eliminated extra space

--- src/gibberish-generator.js
+++ src/gibberish-generator.js
@@ -2,7 +2,7 @@ module.exports = function (args) {
     const PARAGRAPHS = args?.paragraphs || 1; // Number of paragraphs to generate
     const LETTERS = args?.letters || 'abcdefghijklmnopqrstuvwxyz'; // String of letters to generate
     const SEPATATOR = args?.separator || ' '; // Character to use as separator between words. NOTE: This should be a single character
-    const  PARAGRAPHLENGTH = args?.paragraphLength || 100; // Number of characters per paragraph
+    const PARAGRAPHLENGTH = args?.paragraphLength || 100; // Number of characters per paragraph
     const WORDMINLENGTH = args?.wordMinLength || 3; // Number of letters per word
     const WORDMAXLENGTH = args?.wordMaxLength || 8; // Number of letters per word
 
