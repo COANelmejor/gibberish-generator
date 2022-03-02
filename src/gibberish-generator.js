b65d6db45a44b73b17ada6bdeb23bd70877597a4
Christian Arana <coanelmejor@gmail.com>
2022-03-02 10:29:09 -0600

    Completed module. NO doumentation yet

--- src/gibberish-generator.js
+++ src/gibberish-generator.js
@@ -1,19 +1,22 @@
 module.exports = function (args) {
     const PARAGRAPHS = args?.paragraphs || 1; // Number of paragraphs to generate
     const LETTERS = args?.letters || 'abcdefghijklmnopqrstuvwxyz'; // String of letters to generate
-    const SEPATATORS = args?.separators || '.,!?'; // String of separators to generate
-    const PARAGRAPHLENGTH = args?.paragraphLength || 100; // Number of letters per paragraph
-    const WORDMINLENGTH = args?.wordMinLength || 3; // Number of letters per word
-    const WORDMAXLENGTH = args?.wordMaxLength || 8; // Number of letters per word
+    // const SEPATATORS = args?.separators || '.,!?'; // String of separators to generate
+    const PARAGRAPHLENGTH = args?.paragraphLength || 100; // Number of characters per paragraph
+    const WORDMINLENGTH = args?.wordMinLength || 2; // Number of letters per word
+    const WORDMAXLENGTH = args?.wordMaxLength || 12; // Number of letters per word
 
-
-    return {
-        paragraphs: PARAGRAPHS,
-        letters : LETTERS,
-        sepatators : SEPATATORS,
-        paragraphLength : PARAGRAPHLENGTH,
-        wordMinLength : WORDMINLENGTH,
-        wordMaxLength : WORDMAXLENGTH,
-    };
-    
+    const gibberish = [];
+    for (let i = 0; i < PARAGRAPHS; i++) {
+        const paragraph = [];
+        for (let j = 0; j < PARAGRAPHLENGTH; j++) {
+            const word = [];
+            for (let k = 0; k < Math.floor(Math.random() * (WORDMAXLENGTH - WORDMINLENGTH + 1)) + WORDMINLENGTH; k++) {
+                word.push(LETTERS[Math.floor(Math.random() * LETTERS.length)]);
+            }
+            paragraph.push(word.join(''));
+        }
+        gibberish.push(paragraph.join(' ').slice(0, PARAGRAPHLENGTH));
+    }
+    return PARAGRAPHS === 1 ? gibberish[0] : gibberish;
 }
