4bc45e902ec587f9090f7bda4676daa2574c4e20
Christian Arana <coanelmejor@gmail.com>
2022-03-02 10:55:20 -0600

    Added a separator option

--- src/gibberish-generator.js
+++ src/gibberish-generator.js
@@ -1,10 +1,12 @@
 module.exports = function (args) {
     const PARAGRAPHS = args?.paragraphs || 1; // Number of paragraphs to generate
     const LETTERS = args?.letters || 'abcdefghijklmnopqrstuvwxyz'; // String of letters to generate
-    // const SEPATATORS = args?.separators || '.,!?'; // String of separators to generate
-    const PARAGRAPHLENGTH = args?.paragraphLength || 100; // Number of characters per paragraph
-    const WORDMINLENGTH = args?.wordMinLength || 2; // Number of letters per word
-    const WORDMAXLENGTH = args?.wordMaxLength || 12; // Number of letters per word
+    const SEPATATOR = args?.separator || ' '; // Character to use as separator between words. NOTE: This should be a single character
+    const  PARAGRAPHLENGTH = args?.paragraphLength || 100; // Number of characters per paragraph
+    const WORDMINLENGTH = args?.wordMinLength || 3; // Number of letters per word
+    const WORDMAXLENGTH = args?.wordMaxLength || 8; // Number of letters per word
+
+
 
     const gibberish = [];
     for (let i = 0; i < PARAGRAPHS; i++) {
@@ -16,7 +18,7 @@ module.exports = function (args) {
             }
             paragraph.push(word.join(''));
         }
-        gibberish.push(paragraph.join(' ').slice(0, PARAGRAPHLENGTH));
+        gibberish.push(paragraph.join(SEPATATOR).slice(0, PARAGRAPHLENGTH));
     }
     return PARAGRAPHS === 1 ? gibberish[0] : gibberish;
 }
